1188c7f009b0f3f5b2ba22e2d815f8d074a90024
Giacomo Barbieri <jaack@routescan.io>
2026-01-07 15:20:09 +0100

    feat: Remove footer from layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,9 +22,7 @@
 	<slot />
 </main>
 
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
+
 
 <style>
 	main {
@@ -38,21 +36,8 @@
 		box-sizing: border-box;
 	}
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
 
-	footer a {
-		font-weight: bold;
-	}
 
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
+
+
 </style>
