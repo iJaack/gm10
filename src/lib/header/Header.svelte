21d8f3ca393893241c4f57220ef58eb3a839a254
Giacomo Barbieri <jaack@routescan.io>
2026-01-07 15:39:02 +0100

    fix: Correct theme import path and syntax errors in Header

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { page } from '$app/stores';
 	import logo from './svelte-logo.svg';
-import { theme } from '../theme';
+import { theme } from '../../lib/theme'';
 </script>
 
 function toggleTheme() {
