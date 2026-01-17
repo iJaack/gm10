baff8d6ba398e97f7abae92c56deeceb6906cc3e
Giacomo Barbieri <jaack@routescan.io>
2026-01-18 00:44:34 +0100

    fix: ignore porto/internal module causing build failure

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
+const webpack = require('webpack');
+
 const nextConfig = {
     reactStrictMode: true,
     webpack: (config) => {
         config.externals.push("pino-pretty", "lokijs", "encoding");
+        config.plugins.push(
+            new webpack.IgnorePlugin({
+                resourceRegExp: /^porto\/internal$/,
+            })
+        );
         return config;
     },
 };
