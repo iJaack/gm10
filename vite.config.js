14446c8288f328d49810800b0570f4a8fdd8198d
Giacomo Barbieri <jaack@routescan.io>
2026-01-17 14:47:40 +0100

    fix: polyfill global for Web3 libraries in Vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
   plugins: [sveltekit()],
   define: {
-    'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
+    'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
+    global: 'globalThis',
   }
 };
 
